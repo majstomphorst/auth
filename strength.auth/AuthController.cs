139ae49c755d26ad9c400dcdb12908560d69b1c6
maj <maxim.stomphorst@han.nl>
2023-05-21 14:27:22 +0200

    add logger

--- strength.auth/AuthController.cs
+++ strength.auth/AuthController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
 
 namespace strength.auth;
 
@@ -11,14 +12,16 @@ public class AuthController
     [FunctionName("auth")]
     public static async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
-        HttpRequestMessage req)
+        HttpRequestMessage req, ILogger logger)
     {
+        logger.Log(LogLevel.Information, "point hit");
         var result = "no body";
         if (req.Content != null)
         {
             var body = await req.Content.ReadAsStringAsync();
             if (!string.IsNullOrEmpty(body))
             {
+                logger.Log(LogLevel.Information, body);
                 result = body;
             }
         }
